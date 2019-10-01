acaa731675f33881b4fa709ecb4cf9bf46ecc180
jescamilla <corem96@gmail.com>
2019-10-01 00:02:53 -0600

    feat: crea controlador para tarjetas con operaciones CRUD

---
+++ src/Resipass.Api/Api/Tarjeta/TarjetaController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resipass.Data.contexto;
+using TarjetaModel = Resipass.Domain.modelos.Tarjeta.Tarjeta;
+
+namespace Resipass.Api.Api.Tarjeta
+{
+    [Route("api/tarjeta")]
+    public class TarjetaController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private const string InvalidDataString = "invalid data";
+
+        public TarjetaController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(await _dbContext.Tarjetas.ToListAsync());
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] TarjetaModel modelo)
+        {
+            ModelState.Remove("Id");
+            if (!ModelState.IsValid)
+                return BadRequest(new {Error = InvalidDataString});
+
+            try
+            {
+                modelo.Vigencia = DateTime.UtcNow.AddDays(30);
+                
+                _dbContext.Add(modelo);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(modelo);
+            }
+            catch (DbUpdateException e)
+            {
+                ModelState.AddModelError("error", "no es posible insertar los datos");
+                return BadRequest(new {Error = e.Message});
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Actualizar([FromBody] TarjetaModel modelo)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new {Error = InvalidDataString});
+
+            try
+            {
+                _dbContext.Entry(modelo).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(modelo);
+            }
+            catch (DbUpdateException e)
+            {
+                ModelState.AddModelError("error",
+                    "No es posible actualizar datos");
+                return BadRequest(new {Error = e.Message});
+            }
+        }
+    }
+}
