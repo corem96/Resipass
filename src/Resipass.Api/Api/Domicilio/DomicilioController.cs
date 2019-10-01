5657b7d36513b42157cd6c1b7d958c28c31a0f54
jescamilla <corem96@gmail.com>
2019-09-30 23:29:35 -0600

    feat: crea controlador para domicilios con operaciones CRUD

---
+++ src/Resipass.Api/Api/Domicilio/DomicilioController.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resipass.Data.contexto;
+using DomicilioModel = Resipass.Domain.modelos.Domicilio.Domicilio;
+
+namespace Resipass.Api.Api.Domicilio
+{
+    [Route("api/domicilio")]
+    public class DomicilioController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private const string InvalidDataString = "invalid data";
+
+        public DomicilioController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _dbContext.Domicilios.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] DomicilioModel modelo)
+        {
+            ModelState.Remove("Id");
+            if (!ModelState.IsValid)
+                return BadRequest(new {Error = InvalidDataString});
+
+            try
+            {
+                _dbContext.Add(modelo);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(modelo);
+            }
+            catch (DbUpdateException e)
+            {
+                ModelState.AddModelError("error", "no es posible insertar los datos");
+                return BadRequest(new {Error = e.Message});
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Actualizar([FromBody] DomicilioModel modelo)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new {Error = InvalidDataString});
+
+            try
+            {
+                _dbContext.Entry(modelo).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(modelo);
+            }
+            catch (DbUpdateException e)
+            {
+                ModelState.AddModelError("error",
+                    "No es posible actualizar datos");
+                return BadRequest(new {Error = e.Message});
+            }
+        }
+    }
+}
