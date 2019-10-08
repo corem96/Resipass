7dbf2b35f56a448e2b4fb1d01bf94f3998c2c4ac
jescamilla <corem96@gmail.com>
2019-10-07 23:22:58 -0600

    feat: intenta configurar automapper

---
+++ src/Resipass.Api/Config/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+
+namespace Resipass.Api.Config
+{
+    public class ServiceExtensions
+    {
+//        public IConfiguration Configuration { get; set; }
+//
+//        public static void ConfigureAutoMapper(this IServiceCollection services)
+//        {
+//            services.AddScoped<IMapper>(provider =>
+//            {
+//                var config = new MapperConfiguration(cfg =>
+//                {
+//                    
+//                });
+//            });
+//        }
+    }
+}
