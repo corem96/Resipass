5dd5e7288e5b0ecba4ebd6ce7259cc03108a9c61
jescamilla <corem96@gmail.com>
2019-10-15 13:01:47 -0600

    feat: actualiza propiedades de modelo

--- src/Resipass.Domain/modelos/Domicilio/Domicilio.cs
+++ src/Resipass.Domain/modelos/Domicilio/Domicilio.cs
@@ -8,6 +8,7 @@ namespace Resipass.Domain.modelos.Domicilio
         public int Id { get; set; }
         public string Direccion { get; set; }
         public string Numero { get; set; }
+        public string DomicilioCompleto => $"{Direccion} #{Numero}";
 
         [JsonIgnore]
         public ICollection<Residente.Residente> Residentes { get; set; }
